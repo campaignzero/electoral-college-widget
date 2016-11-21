10de0354d2074e93d7330d73530b2fb41e8d5506
Peter Schmalfeldt <peter@manifestinteractive.com>
2016-11-21 16:53:47 -0500

    Add Website that uses Widget for users accessing the web root of our project

---
+++ website/js/custom-scripts.js
@@ -0,0 +1,117 @@
+(function () {
+  function smoothScrolling() {
+
+    $(".navbar-nav a").on("click", function () {
+
+      if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        var offset = $('.header-wrapper').outerHeight();
+
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top - parseInt(offset, 0)
+          }, 500);
+
+          return false;
+        }
+      }
+    });
+  }
+
+  function debounce(func, wait, immediate) {
+    var timeout;
+    return function() {
+      var context = this, args = arguments;
+      var later = function() {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      var callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  }
+
+  var resizeHandeler = debounce(function() {
+    var footerHeight = document.querySelector('.cta-section-3').clientHeight;
+    document.querySelector('.ec-container').style.maxWidth = (window.innerHeight - 100) + 'px';
+    document.querySelector('.ec-widget').style.maxHeight = (window.innerHeight - footerHeight) + 'px';
+  }, 25);
+
+  window.addEventListener('resize', resizeHandeler);
+
+  resizeHandeler();
+  smoothScrolling();
+})();
+
+(function($) {
+  "use strict";
+  $(document).on('ready', function() {
+
+    var $form = $('.signup-form');
+    var $email = $('#mce-EMAIL');
+    var $alert = $('.form-message');
+
+    $email.on('keyup', function() {
+      console.log('email');
+      $alert.removeClass('text-danger text-success').html('').hide();
+    });
+
+    if ($form.length > 0) {
+      console.log('form');
+      $('button[type="submit"]', $form).on('click', function(event) {
+        console.log('submit');
+        if (event) {
+          event.preventDefault()
+        }
+        if (validateInput($form)) {
+          register($form)
+        }
+      })
+    }
+
+    function validateInput($form) {
+
+      var validEmail = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+
+      $alert.removeClass('text-danger text-success').html('').hide();
+
+      if ($email.val() && validEmail.test($email.val())) {
+        return true
+      }
+
+      $alert.html('Please Enter a Valid Email Address').addClass('text-danger').fadeIn(500);
+      return false
+    }
+
+    function register($form) {
+      $.ajax({
+        type: $form.attr('method'),
+        url: $form.attr('action'),
+        data: $form.serialize(),
+        cache: false,
+        dataType: 'json',
+        contentType: "application/json; charset=utf-8",
+        error: function(err) {
+          $alert.html(err).addClass('text-danger').fadeIn();
+        },
+        success: function(data) {
+          if (data.result !== 'success') {
+            $alert.html(data.msg).addClass('text-danger').fadeIn(500);
+          } else {
+            $form.trigger('reset');
+            $alert.html('Woo Hoo!&nbsp; Now, Go Check Your Email :)');
+            $alert.addClass('text-success').fadeIn(500);
+
+            setTimeout(function(){
+              $alert.removeClass('text-danger text-success').html('').hide();
+            }, 10000);
+          }
+        }
+      })
+    }
+  })
+})(jQuery);
